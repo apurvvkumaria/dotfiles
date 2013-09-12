ce9de23ddc3f37340a5b71b4eab344bc9a8607dd
Apurv Kumaria <apurv.kumaria@RACKSPACE.COM>
2013-09-12 17:14:21 -0500

    Changed tmp file's name

--- python/find_all_pep8_errors.sh
+++ python/find_all_pep8_errors.sh
@@ -3,4 +3,4 @@
 
 # cd to the project
 
-find . -iname "*.py" | grep ".*\.py$" | xargs pep8 --exclude=setup.py  > test.log; awk '/.*E[0-9]+.*/' test.log; rm test.log
+find . -iname "*.py" | grep ".*\.py$" | xargs pep8 --exclude=setup.py  > pep8_errors.log; awk '/.*E[0-9]+.*/' pep8_errors.log; rm pep8_errors.log
